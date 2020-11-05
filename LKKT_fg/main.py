88f3db27a8ffe4de3faab09cb6cf4ad1a10ad5cb
nitslaszlo <nitslaszlo@gmail.com>
2020-11-05 09:30:57 +0100

    LKKT_fg first commit

---
+++ LKKT_fg/main.py
@@ -0,0 +1,29 @@
+import random
+#  from random import *  # közvetlenül tudunk a random osztály metódusaira hivatkozni
+
+def lkkt(a: int, b: int) -> int:
+    # ksz: int = min(a, b)
+    # nsz: int = max(a, b)
+    if a > b:
+        nsz: int = a
+        ksz: int = b
+    else:
+        nsz: int = b
+        ksz: int = a
+    #  javítva
+    osztando: int = nsz  # nagyobb szám első többszöröse
+    while osztando % ksz != 0:
+        osztando += nsz  # nagyobb szám következő többszöröse
+    return osztando
+
+
+def main():
+    print("\n\nLKKT függvény hívása véletlen számokkal")
+    a: int = random.randint(1, 999)
+    b: int = random.randint(1, 999)
+    print(f'LKKT({a},{b}) = {lkkt(a, b)}')
+    print(f'A két szám szorzata: {a} * {b} = {a * b}')
+
+
+if __name__ == '__main__':
+    main()
