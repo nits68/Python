14def4ecee24ce0909d5a597efdaf9e0c089068d
László Nits <nitslaszlo@gmail.com>
2020-11-06 22:49:29 +0100

    up

---
+++ Primszamvizsgalat_fg/test_main.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from main import prime
+
+
+class TestMain(TestCase):
+    def test_prime(self):
+        self.assertEqual(prime(1), False)
+        self.assertEqual(prime(2), True)
+        self.assertEqual(prime(3), True)
+        self.assertEqual(prime(4), False)
+        self.assertEqual(prime(5), True)
+        self.assertEqual(prime(6), False)
+        self.assertEqual(prime(7), True)
+        self.assertEqual(prime(8), False)
+        self.assertEqual(prime(9), False)
+        self.assertEqual(prime(10), False)
+        self.assertEqual(prime(11), True)
+        self.assertEqual(prime(12), False)
+        self.assertEqual(prime(13), True)
+        self.assertEqual(prime(14), False)
+        self.assertEqual(prime(15), False)
+        self.assertEqual(prime(16), False)
