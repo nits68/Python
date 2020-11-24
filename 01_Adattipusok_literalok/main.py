980b99ba800808beac12baa1b93fdf4745c3e015
nitslaszlo <nitslaszlo@gmail.com>
2020-11-24 20:32:03 +0100

    up

--- 01_Adattipusok_literalok/main.py
+++ 01_Adattipusok_literalok/main.py
@@ -1,10 +1,12 @@
+from typing import Dict, List, Set, Tuple
+
 # program, algoritmus, változó, adatszerkezet, literál
 # =====================================================
 # program = algoritmus + adatszerkezet
 # algoritmus = utasítások végrehajtásának sorrendje, amit vezérlési szerkezetek határoznak meg a programozási nyelvekben
 # változók = adatok tárolására használt egységek
 # változók jellemzői:
-# - azonosító (InputTime C#, input_time Python)
+# - azonosító (változó neve)
 # - adattípus (str, int, float, bool, stb., Python-ban megadásuk opcionális)
 # - kezdőérték
 # - aktuális érték
@@ -19,12 +21,10 @@
 
 # 1. Egyszerű adattípusok: Egy érték tárolására alkalmasak
 # 1.1 Szöveges adattípus és literál: str
-from typing import List, Set, Tuple
 
-
-szöveg: str = 'Python'
-szöveg2: str = "Jedlik"
-print(type(szöveg))
+szoveg: str = 'Python'
+szoveg2: str = "Jedlik"
+print(type(szoveg))  # type() beépített függvény a változó típusát "adja vissza"
 
 # 1.2 Numerikus adattípusok: int, float és a literálok
 egesz: int = 68  # Egész érték tárolásához
@@ -48,7 +48,11 @@ print(tuple)
 halmaz: Set[str] = {'apple', 'banana', 'cherry'}
 print(halmaz)
 
-# 2.4 Számsorozat (range, for ciklusokhoz használjuk általában)
+# 2.4 Szótár (dict, kulcs és érték (értékpárok), kulcs egyedi)
+szotar: Dict[str, int] = {"alma": 20, "körte": 23, "barack": 33}
+print(szotar)
+
+# 2.5 Számsorozat (range, for ciklusokhoz használjuk általában)
 sor1: range = range(6)
 print(sor1, end=' - ')
 for i in sor1:
