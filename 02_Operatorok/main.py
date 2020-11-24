91c918c032b5a753c7b556fc47a1b083e5c99cc1
nitslaszlo <nitslaszlo@gmail.com>
2020-11-24 20:38:49 +0100

    up

---
+++ 02_Operatorok/main.py
@@ -0,0 +1,17 @@
+
+# URL: https://www.w3schools.com/python/python_operators.asp
+
+# Nem kellenek a w3s-oldalról:
+
+# Python Assignment Operators:
+# %=
+# //=
+# **=
+# &=
+# |=
+# ^=
+# >>=
+# <<=
+
+# Python Bitwise Operators:
+# Egyik sem kell!
