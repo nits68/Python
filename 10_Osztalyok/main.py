c927c92551c00eecd03127d44ec5cfdd5938a7c3
László Nits <nitslaszlo@gmail.com>
2020-11-28 22:09:02 +0100

    up

--- 10_Osztalyok/main.py
+++ 10_Osztalyok/main.py
@@ -4,35 +4,37 @@ print('Osztályok - objektumok')
 # A függvényeket gyakran metódusoknak hívjuk
 # Az osztály változóit (adattagjait) gyakran mezőknek is hívjuk
 # Az adat- és kódtagok láthatósági szintjét (public, private, protected, stb.)
-# a Python nem különbözteti meg, a protected tagok jelölésére van csak módunk
+#   a Python nem különbözteti meg, a protected tagok jelölésére van csak módunk
 # Egy osztályt leggyakrabban csak példányosítás után használhatunk
 # Az osztály konstruktora egy speciális metódus, jellemzően az adattagok inicializálását végzi,
-# felkészíti az osztálypéldányt a használatra
+#   felkészíti az osztálypéldányt a használatra
 # A konstruktor automatikusan hívódik az osztálypéldány létrehozásakor
 # A konstruktor Pythonban kötelezően az __init__ nevet kapja
 # Az osztálypéldányt gyakran objektumnak is hívjuk
-# A jellemzők speciális kódtagok, gyakran egy privát mező írását és olvasását felügyelik
+# A jellemzők speciális kódtagok, gyakran egy protected mező írását és olvasását felügyelik
 # A self foglalt szóval az aktuális osztálypéldány adat- és kódtagjait érjük el,
-# kötelezően minden kódtag első paramétere
+#   kötelezően minden kódtag első paramétere
+
+# A Homerseklet osztály definiálása:
 
 
 class Homerseklet:  # a class foglalt szó után adjuk meg az osztály azonosítóját (nevét)
-    _ertekfok: float  # adattag (mező) protected jelzéssel (_), csak az osztály kódtagjai érhetik el használhatják)
-    feldolgozas_alatt: bool  # publikus mező, osztálypéldány felől is elérhető
+    _ertekfok: float  # adattag (mező) protected jelzéssel (_), csak az osztály kódtagjai érhetik el, használhatják
+    feldolgozas_alatt: bool  # publikus mező, kódtagokból és osztálypéldány felől is elérhető
 
-    def __init__(self, ertek_fok: float = 0) -> None:
+    def __init__(self, ertek_fok: float = 0) -> None:  # az osztály konstruktora, speciális metódusa
         self.ertekfok = ertek_fok
         self.feldolgozas_alatt = False
 
-    def valtoztat(self, delta_fok: float) -> None:
-        self._ertekfok += delta_fok
+    def valtoztat(self, delta_fok: float) -> None:  # az osztály publikus kódtagja, metódusa
+        self.ertekfok += delta_fok
 
     @property
     def ertekfahrenheit(self) -> float:  # csak olvasható jellemző (nincs setter)
         return (self._ertekfok * 1.8) + 32
 
     @property
-    def ertekfok(self) -> float:  # írható / olvasható jellemző
+    def ertekfok(self) -> float:  # írható / olvasható jellemző (van setter)
         return self._ertekfok
 
     @ertekfok.setter
@@ -45,15 +47,25 @@ class Homerseklet:  # a class foglalt szó után adjuk meg az osztály azonosít
 testho: Homerseklet = Homerseklet(37)  # osztálypéldány (objektum) létrehozása
 
 print(testho.feldolgozas_alatt)  # a publikus tagok osztálypéldány felől is elérhetőek
-# print(testho._ertekfok)  # Pylance figyelmeztetés: "_ertekfok" is protected and used outside of the class in which it is declaredPylance (reportPrivateUsage)
+# print(testho._ertekfok)  # Pylance figyelmeztetés:
+# "_ertekfok" is protected and used outside of the class in which it is declaredPylance (reportPrivateUsage)
 
 print(testho.ertekfok)  # jellemző olvasása
 testho.ertekfok = 36.4  # jellemző írása
 print(testho.ertekfahrenheit)  # jellemző olvasása
-# testho.ertekfahrenheit = 100  # Cannot assign member "ertekfahrenheit" for type "Homerseklet"  Property "ertekfahrenheit" has no defined setterPylance
+# testho.ertekfahrenheit = 100  # jellemző írása - Pylance figyelmeztetés:
+# Cannot assign member "ertekfahrenheit" for type "Homerseklet"  Property "ertekfahrenheit" has no defined setterPylance
 
 try:
     proba: Homerseklet = Homerseklet(-300)  # osztálypéldány (objektum) létrehozása
 except Exception as ex:
-    print(f'Hiba doc: {ex.__doc__}')
+    print(f'Hiba __doc__: {ex.__doc__}')
+    print(f'Hibaüzenet: {ex.args[0]}')
+
+try:
+    while True:
+        testho.valtoztat(-50)  # osztály metódusának hívása (paramétere csak metódusnak lehet)
+        print(testho.ertekfok)
+except Exception as ex:
+    print(f'Hiba __doc__: {ex.__doc__}')
     print(f'Hibaüzenet: {ex.args[0]}')
