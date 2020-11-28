99cda6b86fdd34a3e696e274b93673e022288fa4
László Nits <nitslaszlo@gmail.com>
2020-11-28 01:27:45 +0100

    up

--- 03_Vezerlesi_szerkezetek/01_Szekvencia/main.py
+++ 03_Vezerlesi_szerkezetek/01_Szekvencia/main.py
@@ -1,14 +1,14 @@
-﻿# szekvencia = utasítások végrehajtása fentről-lefelé (top-down)
+﻿# szekvencia vezérlési szerkezet: utasítások végrehajtása fentről-lefelé (top-down)
 # Az utasítások végrehajtásának sorrendjét a "leírás sorrendje" határozza meg
 
-# Csak szekvenciát tartalmazó egyszerű program
-# (Téglalap kerület-terület meghatározás)
+# Csak szekvenciát tartalmazó egyszerű program: Téglalap kerület-terület meghatározás
 
+# print() függvény: változók, literálok és kifejezések értkének megjelenítése a console ablakban
 print('Téglalap kerülete és területe')
 print('Adja meg a téglalap oldalait!')
 oldal_a: float = float(input('a= '))
 # szöveges típusú adat bekérése: input('a= ')
-# szöveges típusú adat konvertálása valós számra: float("3.14")
+# szöveges típusú adat konvertálása valós számra: float('3.14')
 # programban használt operátorok:
 # "+" - összeadás
 # "*" - szorzás
