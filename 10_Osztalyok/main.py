d81e74dd9332ab250c0b8705891d830e581676b0
László Nits <nitslaszlo@gmail.com>
2020-11-28 21:39:16 +0100

    up

---
+++ 10_Osztalyok/main.py
@@ -0,0 +1,59 @@
+print('Osztályok - objektumok')
+# Az osztály a programozási nyelvek legfontosabb összetett adattípusai
+# Biztosítják az adatok és a rajtuk műveletet végző függvények (kódtagok) egységét
+# A függvényeket gyakran metódusoknak hívjuk
+# Az osztály változóit (adattagjait) gyakran mezőknek is hívjuk
+# Az adat- és kódtagok láthatósági szintjét (public, private, protected, stb.)
+# a Python nem különbözteti meg, a protected tagok jelölésére van csak módunk
+# Egy osztályt leggyakrabban csak példányosítás után használhatunk
+# Az osztály konstruktora egy speciális metódus, jellemzően az adattagok inicializálását végzi,
+# felkészíti az osztálypéldányt a használatra
+# A konstruktor automatikusan hívódik az osztálypéldány létrehozásakor
+# A konstruktor Pythonban kötelezően az __init__ nevet kapja
+# Az osztálypéldányt gyakran objektumnak is hívjuk
+# A jellemzők speciális kódtagok, gyakran egy privát mező írását és olvasását felügyelik
+# A self foglalt szóval az aktuális osztálypéldány adat- és kódtagjait érjük el,
+# kötelezően minden kódtag első paramétere
+
+
+class Homerseklet:  # a class foglalt szó után adjuk meg az osztály azonosítóját (nevét)
+    _ertekfok: float  # adattag (mező) protected jelzéssel (_), csak az osztály kódtagjai érhetik el használhatják)
+    feldolgozas_alatt: bool  # publikus mező, osztálypéldány felől is elérhető
+
+    def __init__(self, ertek_fok: float = 0) -> None:
+        self.ertekfok = ertek_fok
+        self.feldolgozas_alatt = False
+
+    def valtoztat(self, delta_fok: float) -> None:
+        self._ertekfok += delta_fok
+
+    @property
+    def ertekfahrenheit(self) -> float:  # csak olvasható jellemző (nincs setter)
+        return (self._ertekfok * 1.8) + 32
+
+    @property
+    def ertekfok(self) -> float:  # írható / olvasható jellemző
+        return self._ertekfok
+
+    @ertekfok.setter
+    def ertekfok(self, ertek_fok: float) -> None:
+        if ertek_fok < -273.15:
+            raise ValueError('-273 foknál kisebb nem lehetséges')
+        self._ertekfok = ertek_fok
+
+
+testho: Homerseklet = Homerseklet(37)  # osztálypéldány (objektum) létrehozása
+
+print(testho.feldolgozas_alatt)  # a publikus tagok osztálypéldány felől is elérhetőek
+# print(testho._ertekfok)  # Pylance figyelmeztetés: "_ertekfok" is protected and used outside of the class in which it is declaredPylance (reportPrivateUsage)
+
+print(testho.ertekfok)  # jellemző olvasása
+testho.ertekfok = 36.4  # jellemző írása
+print(testho.ertekfahrenheit)  # jellemző olvasása
+# testho.ertekfahrenheit = 100  # Cannot assign member "ertekfahrenheit" for type "Homerseklet"  Property "ertekfahrenheit" has no defined setterPylance
+
+try:
+    proba: Homerseklet = Homerseklet(-300)  # osztálypéldány (objektum) létrehozása
+except Exception as ex:
+    print(f'Hiba doc: {ex.__doc__}')
+    print(f'Hibaüzenet: {ex.args[0]}')
