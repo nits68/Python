541fee7a78c5ba8734aa8eb61ca0544aec4fe536
László Nits <nitslaszlo@gmail.com>
2020-11-29 07:52:30 +0100

    up

--- 09_Filekezeles/main.py
+++ 09_Filekezeles/main.py
@@ -4,13 +4,14 @@ from typing import List
 print('Szövges állományok kezelése')
 
 # UTF-8 kódolású szöveges állományok olvasása:
+# try-except szerkezet elhagyható, ha a vizsgafeladatban nem kérik, de a gyakorlatban erősen javasolt a használata
 # (Az állomány sorait egy str típusú listába olvassuk be az open() függvénnyel és a with-as utasítással:
 sorok: List[str] = list()
-with open('forras.txt', 'r', encoding='UTF-8') as file:
-    try:
+try:
+    with open('forras.txt', 'r', encoding='UTF-8') as file:
         sorok = file.read().splitlines()
-    except Exception as ex:
-        print(f'Hiba: {ex.__doc__}')
+except Exception as ex:
+    print(f'Hiba: {ex.__doc__}')
 print(sorok)
 
 # Az open() függvény második paramétere a megnyitás módját határozza meg:
@@ -21,16 +22,18 @@ print(sorok)
 
 # Szöveges állokmányok írása
 # - írás soronként a write() metódussal:
-with open('cél.txt', 'w', encoding='UTF-8') as file:
-    try:
+
+try:
+    with open('cél.txt', 'w', encoding='UTF-8') as file:
         for e in sorok:
-            file.write(f'{e}\n')
-    except Exception as ex:
-        print(f'Hiba: {ex.__doc__}')
+            file.write(f'{e}\n')  # A sorvég '\n' vezérlő karakter hozzáfűzése általában szükséges
+except Exception as ex:
+    print(f'Hiba: {ex.__doc__}')
 
 # - teljes lista kiírása a writelines() metódussal (a '\n' vezérlő karaktereket minden sor végéhez kell fűzni):
-with open('cél2.txt', 'w', encoding='UTF-8') as file:
-    try:
+
+try:
+    with open('cél2.txt', 'w', encoding='UTF-8') as file:
         file.writelines([f'{e}\n' for e in sorok])
-    except Exception as ex:
-        print(f'Hiba: {ex.__doc__}')
+except Exception as ex:
+    print(f'Hiba: {ex.__doc__}')
