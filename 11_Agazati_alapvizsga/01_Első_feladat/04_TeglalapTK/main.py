37b5a3816d02d8f1d88338718fece9cde7c87fc7
László Nits <nitslaszlo@gmail.com>
2020-12-04 20:23:29 +0100

    up

---
+++ 11_Agazati_alapvizsga/01_Első_feladat/04_TeglalapTK/main.py
@@ -0,0 +1,15 @@
+# Kérje be egy téglalap oldalait és tárolja őket valós|egész típusú változókba,
+# majd határozza meg, a téglalap területét és kerületét!
+# A területet és a kerületet a következő képletekkel számolja:
+# T = a * b
+# K = 2 * (a + b)
+
+# Egy lehetséges megoldás:
+print('Téglalap kerülete és területe')
+print('Adja meg a téglalap oldalait!')
+a: float = float(input('a= '))
+b: float = float(input('b= '))
+terulet: float = a * b
+kerulet: float = 2 * (a + b)
+print(f'T = {terulet}')
+print(f'K = {kerulet}')
---
+++ 11_Agazati_alapvizsga/01_Első_feladat/05_Lnko/main.py
@@ -0,0 +1,17 @@
+# Határozza meg két egész szám legnagyobb közös osztóját a következő
+# algoritmussal: Mindaddig kisebbítse a nagyobb számot a kisebb számmal,
+# amíg a két szám egyenlő nem lesz! Az így kapott szám lesz a
+# legnagyobb közös osztó.
+# A számok bekérését és az eredmény kiírását a minta szerint végezze!
+
+# Egy lehetséges megoldás:
+print('LNKO kivonásos algoritmussal')
+a: int = int(input('a = '))
+b: int = int(input('b = '))
+print(f'LNKO({a},{b}) = ', end='')
+while a != b:
+    if a > b:
+        a = a - b
+    else:
+        b = b - a
+print(a)
