364437c2a939b4cd3d6ee5de60a2b990466ac724
nitslaszlo <nitslaszlo@gmail.com>
2021-11-11 14:33:48 +0100

    up

--- 05_Kollekciok/02_Halmazok/01_Lottoszamok/main.py
+++ 05_Kollekciok/02_Halmazok/01_Lottoszamok/main.py
@@ -1,16 +1,15 @@
 import random
-from typing import List, Set
 
 
 def main() -> None:
     # Elöltesztelő ciklus példa: lottószámok generálása
-    lotto90: Set[int] = set()  # Halmaz
+    lotto90: set[int] = set()  # Halmaz
     while len(lotto90) < 5:
         lotto90.add(random.randint(1, 90))  # 1 >= vélszám <= 90
     print(lotto90)
 
     # Halmaz elemei növekvő sorrendben tartalmazás vizsgálattal:
-    lotto90rendezve: List[int] = []
+    lotto90rendezve: list[int] = []
     for szam in range(1, 91):  # 1..90
         if szam in lotto90:
             lotto90rendezve.append(szam)
